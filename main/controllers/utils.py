d66f5c1d41954a10d832acafc02636e0ee8626c8
jungwoo <westhyena@gmail.com>
2020-09-16 15:51:32 +0900

    chore(utils): remove unnecessary print

--- main/controllers/utils.py
+++ main/controllers/utils.py
@@ -1,5 +1,4 @@
 def convert_query_to_response(attrs, elems):
-  print(isinstance(elems, list))
   if isinstance(elems, list):
     return [{attr: col for attr, col in zip(attrs, elem)} for elem in elems]
   else:
