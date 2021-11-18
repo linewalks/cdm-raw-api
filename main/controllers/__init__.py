3e2374bf931d45254c35870a36da905fcb03233e
jungwoo <westhyena@gmail.com>
2021-11-18 15:55:02 +0900

    feat(ml): add create death model

--- main/controllers/__init__.py
+++ main/controllers/__init__.py
@@ -6,11 +6,14 @@ from main.controllers.drug import *
 from main.controllers.measurement import *
 from main.controllers.procedure import *
 
+from main.controllers.ml import *
+
 bps = [
     person_bp,
     visit_bp,
     condition_bp,
     drug_bp,
     measurement_bp,
-    procedure_bp
+    procedure_bp,
+    ml_bp
 ]
